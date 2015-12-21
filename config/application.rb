d15b1f3979726d4ca0c20c872b05845205367192
jondmk <jondmk1208@gmail.com>
2015-12-20 22:21:23 -0800

    Add devise and pin/user association

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module PinBoard
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Needed for Devise to deploy on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
